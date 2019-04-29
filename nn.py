19c82bdca0b1e8aa6b2149958ce14934c8c4ba70
JAJAmoa <jj@hgeek.net>
2019-04-29 23:39:24 +0800

    update

--- nn.py
+++ nn.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 
-data = pd.read_csv("sample.csv")
-X = data.values[:,1:3]
-Y = data.values[:,4:9]
+data = pd.read_csv("Artificial_Data.csv")
+Y = data.values[:,7:8]
+X = data.values[:,1:7]
 
 X_mlb = MultiLabelBinarizer()
 X_mlb.fit(X)
@@ -37,39 +37,44 @@ Y_df = pd.DataFrame(Y)
 print(X_df.head(5))
 print(Y_df.head(5))
 
-
-
 X_train, X_test, Y_train, Y_test = train_test_split(X_df, Y_df, test_size = 0.33, random_state=42)
 print("X_train_shape:", X_train.shape)
 print("Y_train_shape", Y_train.shape)
 print("X_test_shape", X_test.shape)
 print("Y_test_shape", Y_test.shape)
 
-model=Sequential([Dense(10, input_dim=X_train.shape[1]),
-                  Activation('relu'),
-                  Dense(24),
+model=Sequential([Dense(X_train.shape[1], input_dim=X_train.shape[1]),
                   Activation('relu'),
                   Dense(50),
                   Activation('relu'),
-                  Dense(50),
+                  Dense(64),
                   Activation('relu'),
-                  Dense(30),
-                  Activation('tanh'),
+                  Dense(32),
+                  Activation('relu'),
+                  Dense(20),
+                  Activation('relu'),
                   Dense(Y_train.shape[1]),
-                  Activation('sigmoid')])
+                  Activation('softmax')])
 
 model.summary()
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss="binary_crossentropy", optimizer=sgd, metrics=["accuracy"])
-model.fit(X_train,Y_train,epochs=10,batch_size=4)
+model.fit(X_train,Y_train,epochs=30,batch_size=2)
 
 loss, accuracy = model.evaluate(X_test,Y_test)
 print("test loss:",loss)
 print("test accuracy:", accuracy)
-prob = model.predict(X_test.head(500))
+prob = model.predict(X_test.head(10))
 #print(prob[:10,:])
-idxs = np.argsort(-prob)[:10,:5]
+idxs = np.argsort(-prob)[:,:3]
 
-for (i, j) in enumerate(idxs):
-    print(Y_mlb.classes_[j], ":",prob[i][j] * 100,"\n")
+X_test = np.array(X_test)
+#print(X_test.shape[1])
+#print(len(X_mlb.classes_))
+np.set_printoptions(suppress=True)
+for i in range(idxs.shape[0]):
+    print(X_mlb.inverse_transform(X_test)[i])
+    for j in range(idxs.shape[1]):
+        print(Y_mlb.classes_[idxs[i][j]], ":",np.around(prob[i][idxs[i][j]]*100,4),end="% ")
+    print("\n")
